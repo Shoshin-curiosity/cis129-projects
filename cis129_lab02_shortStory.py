3acc68a864efdc5aa53e40f10f4b89245acb62d0
Shoshin <dbierman1@mail.pima.edu>
2025-02-10 16:55:49 -0700

    Update cis129_lab02_shortStory.py
    Deleted print('Hello, World!')
    Used comments to document my name, a brief description of the program behavior, and a summary of the changes made in this commit.
    Added a print() statement to create a short text-based story.
    Added a Print a short story and used a soft wrap to make it into a readable paragraph.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,10 @@
-print('Hello, World!')
+#Author: Desiree Bierman
+
+#My first Python lab in my CIS129 course
+
+#Prints a simple short story
+
+print('One night we thought we heard knocking on the side of the house')
+
+print('''My partner and her 19 year old daughter decided to each get a weapon of choice as I stood back watching this entire scene take place. They both crept to the garage door to make sure the garage was shut. As soon as they opened the door I jumped out and screamed, and scared the living daylights out of both of them. I thought it was hilarious, but they were not very enthralled by my prank. It was probably because they were both holding a weapon and almost went after me when I screamed, hehe. Luckily there wasn't anyone there and no one was injured.''')
+
